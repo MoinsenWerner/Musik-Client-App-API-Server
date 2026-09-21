c524eaeede6b6ad17a35a08ea03997553b35eb3c
PlsReload <114494021+MoinsenWerner@users.noreply.github.com>
2026-09-21 11:38:07 +0200

    Fix Asset Links formatting

--- auth/app.py
+++ auth/app.py
@@ -231,7 +231,10 @@ def create_app(config: dict[str, Any] | None = None) -> Flask:
         return jsonify(
             [
                 {
-                    "relation": [\n                        "delegate_permission/common.get_login_creds",\n                        "delegate_permission/common.handle_all_urls",\n                    ],
+                    "relation": [
+                        "delegate_permission/common.get_login_creds",
+                        "delegate_permission/common.handle_all_urls",
+                    ],
                     "target": {
                         "namespace": "android_app",
                         "package_name": app.config["ANDROID_APP_PACKAGE"],
